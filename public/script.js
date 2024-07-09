c495c137ee0fa4deca20abdc0cec453c17d230ad
iftharkamal <iftharkamal@gmail.com>
2024-07-09 19:29:59 +0530

    js error fixed

--- public/script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector (".toggle-button");
-const dropdown = document.querySelector (".dropdown-menu");
-
-    toggleBtn.addEventListener("click", ()=>{
-        dropdown.classList.toggle('top-16');
-    })
